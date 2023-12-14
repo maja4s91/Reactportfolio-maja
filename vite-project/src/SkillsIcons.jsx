33cbc1df2d21a0717b7c8d94f5fd10e5bb975f70
maja4s91 <majastojanovic4@gmail.com>
2023-12-14 14:58:33 +0100

    Skills Icons Component added

---
+++ vite-project/src/SkillsIcons.jsx
@@ -0,0 +1,15 @@
+import { IonIcon } from "@ionic/react";
+import dataSkills from "./dataSkills";
+
+export default function SkillsIcons() {
+  return (
+    <ul className="skills-container">
+      {dataSkills.map((d) => (
+        <li className="skills-list" key={d.id}>
+          <IonIcon className="ion-icon skills-ion-icon" icon={d.icon}></IonIcon>
+          <h3 className="heading-tertiary h-skills">{d.title}</h3>
+        </li>
+      ))}
+    </ul>
+  );
+}
---
+++ vite-project/src/dataSkills.js
@@ -0,0 +1,56 @@
+import {
+  logoReact,
+  logoHtml5,
+  logoCss3,
+  logoJavascript,
+  logoGithub,
+  logoWordpress,
+  logoFigma,
+  logoElectron,
+} from "ionicons/icons";
+
+const dataSkills = [
+  {
+    id: 9,
+    icon: logoReact,
+    title: "React",
+  },
+  {
+    id: 10,
+    icon: logoJavascript,
+    title: "JavaScript",
+  },
+  {
+    id: 11,
+    icon: logoWordpress,
+    title: "WordPress",
+  },
+  {
+    id: 12,
+    icon: logoHtml5,
+    title: "HTML",
+  },
+  {
+    id: 13,
+    icon: logoCss3,
+    title: "CSS",
+  },
+
+  {
+    id: 14,
+    icon: logoGithub,
+    title: "GitHub",
+  },
+  {
+    id: 15,
+    icon: logoFigma,
+    title: "Figma",
+  },
+  {
+    id: 16,
+    icon: logoElectron,
+    title: "Redux",
+  },
+];
+
+export default dataSkills;
