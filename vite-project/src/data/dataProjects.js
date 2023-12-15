5e25805e1c11d895a674d5beb15301caf583a03c
maja4s91 <majastojanovic4@gmail.com>
2023-12-15 17:06:32 +0100

    Project page styled

---
+++ vite-project/src/data/dataProjects.js
@@ -0,0 +1,46 @@
+import { eyeOutline } from "ionicons/icons";
+
+const dataProjects = [
+  {
+    id: 17,
+    sourceWebP: "../imgs/React-img.webp",
+    sourcePng: "../imgs/React-img.png",
+    title: "React shop",
+    text: "The responsive e-commerce application.",
+    icon: eyeOutline,
+    link: "https://master--reactshop-maja.netlify.app/",
+    titleIcon: "View",
+  },
+  {
+    id: 18,
+    sourceWebP: "../imgs/Blog-img.webp",
+    sourcePng: "../imgs/Blog-img.png",
+    title: "Blog",
+    text: "The blog application with custom blog uploads.",
+    icon: eyeOutline,
+    link: "https://blog-maja.netlify.app/",
+    titleIcon: "View",
+  },
+
+  {
+    id: 19,
+    sourceWebP: "../imgs/Forkify-img.webp",
+    sourcePng: "../imgs/Forkify-img.png",
+    title: "Forkify",
+    text: "The recipe application with custom recipe uploads.",
+    icon: eyeOutline,
+    link: "https://forkify-maja.netlify.app/",
+    titleIcon: "View",
+  },
+  {
+    id: 20,
+    sourceWebP: "../imgs/Omnifood-img.webp",
+    sourcePng: "../imgs/Omnifood-img.png",
+    title: "Omnifood",
+    text: "The responsive website for creating a custom meal plan.",
+    icon: eyeOutline,
+    link: "https://omnifood-maja.netlify.app/",
+    titleIcon: "View",
+  },
+];
+export default dataProjects;
--- vite-project/src/pages/Projects.jsx
+++ vite-project/src/pages/Projects.jsx
@@ -1,105 +1,43 @@
+import SocialMediaIcons from "../components/SocialMediaIcons";
+import dataProjects from "../data/dataProjects";
+import { IonIcon } from "@ionic/react";
+
 const Projects = () => {
   return (
     <section className="section-projects">
       <h2 className="heading-primary">Projects</h2>
+      {dataProjects.map((d) => (
+        <figure className="project" key={d.id}>
+          <picture className="project-img">
+            <source srcSet={d.sourceWebP} type="image/webp" />
+            <source srcSet={d.sourcePng} type="image/png" />
+            <img src={d.sourceWebP} alt={d.title} className="project-img" />
+          </picture>
 
-      <picture className="project-img">
-        <source srcSet="./imgs/Blog-img.webp" type="image/webp" />
-        <source srcSet="./imgs/Blog-img.png" type="image/png" />
-        <img
-          src="./imgs/Blog-img.webp"
-          alt="Blog project"
-          className="project-img"
-        />
-      </picture>
-
-      <div className="text-box">
-        <h3 className="heading-tertiary">Blog</h3>
-        <p className="aboutme-text project-text">
-          The blog application with custom blog uploads.
-        </p>
-        <a
-          className="about-cta"
-          href="https://blog-maja.netlify.app/"
-          target="_blank"
-        >
-          View project&nbsp;&nbsp;➔
-        </a>
-      </div>
-
-      <div className="text-box">
-        <h3 className="heading-tertiary">Meme Generator</h3>
-        <p className="aboutme-text project-text">
-          The responsive Meme Generator. The application generates a new Meme
-          simply by clicking the button. Also, custom text could be displayed on
-          the top and the bottom of each meme.
-        </p>
-        <a
-          className="about-cta"
-          href="https://memegenerator-maja.netlify.app/"
-          target="_blank"
-        >
-          View project&nbsp;&nbsp;➔
-        </a>
-      </div>
-
-      <picture className="project-img">
-        <source srcSet="./imgs/Meme-generator.webp" type="image/webp" />
-        <source srcSet="./imgs/Meme-generator.png" type="image/png" />
-        <img
-          src="./imgs/Meme-generator.png"
-          alt="Meme Generator project"
-          className="project-img"
-        />
-      </picture>
-
-      <picture className="project-img">
-        <source srcSet="./imgs/Forkify-img.webp" type="image/webp" />
-        <source srcSet="./imgs/Forkify-img.png" type="image/png" />
-        <img
-          src="./imgs/Forkify-img.png"
-          alt="Forkify project"
-          className="project-img"
-        />
-      </picture>
-
-      <div className="text-box">
-        <h3 className="heading-tertiary">Forkify</h3>
-        <p className="aboutme-text project-text">
-          The recipe application with custom recipe uploads.
-        </p>
-        <a
-          className="about-cta"
-          href="https://forkify-maja.netlify.app"
-          target="_blank"
-        >
-          View project&nbsp;&nbsp;➔
-        </a>
-      </div>
-
-      <div className="text-box">
-        <h3 className="heading-tertiary">Omnifood</h3>
-        <p className="aboutme-text project-text">
-          The responsive website template for creating a custom, well-being and
-          individual weekly meal plan.
-        </p>
-        <a
-          className="about-cta"
-          href="https://omnifood-maja.netlify.app/"
-          target="_blank"
-        >
-          View project&nbsp;&nbsp;➔
-        </a>
-      </div>
-      <picture className="project-img">
-        <source srcSet="./imgs/Omnifood-img.webp" type="image/webp" />
-        <source srcSet="./imgs/Omnifood-img.png" type="image/png" />
-        <img
-          src="./imgs/Omnifood-img.png"
-          alt="Omnifood project"
-          className="project-img"
-        />
-      </picture>
+          <div className="text-box">
+            <h3 className="heading-tertiary">{d.title}</h3>
+            <p className="aboutme-text project-text">{d.text}</p>
+            {/* <a
+              className="about-cta"
+              href="https://blog-maja.netlify.app/"
+              target="_blank"
+            > 
+              View project&nbsp;&nbsp;➔
+            </a> */}
+            <ul className="profiles-list ">
+              <li className="li-logo" key={d.id}>
+                <a href={d.link} className="profile-link " target="_blank">
+                  <IonIcon
+                    icon={d.icon}
+                    className="ion-icon"
+                    title={d.titleIcon}
+                  ></IonIcon>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </figure>
+      ))}
 
       <a
         className="about-cta projects-view"
