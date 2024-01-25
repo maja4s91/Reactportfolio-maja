2b321488c9f63a695d60d5efa611d6d98ef16c2e
maja4s91 <majastojanovic4@gmail.com>
2024-01-25 20:11:50 +0100

    Skills flex-center added

--- vite-project/src/pages/Skills.jsx
+++ vite-project/src/pages/Skills.jsx
@@ -3,15 +3,17 @@ import { motion } from "framer-motion";
 
 const Skills = () => {
   return (
-    <motion.section
-      className="section-skills"
-      initial={{ opacity: 0, scale: 0.5 }}
-      animate={{ opacity: 1, scale: 1 }}
-      transition={{ duration: 1 }}
-    >
-      <h2 className="heading-primary">Skills</h2>
-      <SkillsIcons />
-    </motion.section>
+    <div className="flex-center">
+      <motion.section
+        className="section-skills"
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 1 }}
+      >
+        <h2 className="heading-primary">Skills</h2>
+        <SkillsIcons />
+      </motion.section>
+    </div>
   );
 };
 
