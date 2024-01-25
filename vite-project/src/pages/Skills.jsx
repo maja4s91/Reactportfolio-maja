7b8a7c6276a14e952ab2b52b7c66275b81157035
maja4s91 <majastojanovic4@gmail.com>
2024-01-25 20:52:08 +0100

    body, html height =100% removed

--- vite-project/src/pages/Skills.jsx
+++ vite-project/src/pages/Skills.jsx
@@ -3,17 +3,15 @@ import { motion } from "framer-motion";
 
 const Skills = () => {
   return (
-    <div className="flex-center">
-      <motion.section
-        className="section-skills"
-        initial={{ opacity: 0, scale: 0.5 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 1 }}
-      >
-        <h2 className="heading-primary">Skills</h2>
-        <SkillsIcons />
-      </motion.section>
-    </div>
+    <motion.section
+      className="section-skills"
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 1 }}
+    >
+      <h2 className="heading-primary">Skills</h2>
+      <SkillsIcons />
+    </motion.section>
   );
 };
 
